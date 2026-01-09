e063f31b730f9147570a5eb17557ff7559ad2c1b
Sku <alexandre.hamad@hotmail.fr>
2026-01-09 16:39:07 +0100

    first commit

---
+++ inc/minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+
+#endif
