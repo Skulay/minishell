94fe052bd163d73f949fdfd7e91f32f7fa5696bc
Sku <alexandre.hamad@hotmail.fr>
2026-01-10 18:08:39 +0100

    prompt tuning

--- src/main/main.c
+++ src/main/main.c
@@ -6,11 +6,119 @@
 /*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 12:07:43 by alehamad          #+#    #+#             */
-/*   Updated: 2026/01/10 17:15:48 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/01/10 18:06:45 by alehamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = ft_strlen(src);
+	if (size == 0)
+	{
+		return (j);
+	}
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (j);
+}
+
+static int	is_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s1 || !set)
+		return (NULL);
+	i = 0;
+	while (s1[i] && is_set(s1[i], set))
+		i++;
+	j = ft_strlen(s1);
+	while (j > i && is_set(s1[j - 1], set))
+		j--;
+	str = (char *)malloc(sizeof(char) * (j - i + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1 + i, j - i + 1);
+	return (str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	dst = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!dst)
+		return (0);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dst[i + j] = s2[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
+
+char	*ft_get_prompt(char **env)
+{
+	char	*prompt;
+	char	*name;
+	char	*path;
+
+	name = getenv("USER");
+	path = getenv("PWD");
+	path = ft_strtrim(path, "/home");
+	path = ft_strtrim(path, name);
+	prompt = ft_strjoin(name, " ~");
+	prompt = ft_strjoin(prompt, path);
+	prompt = ft_strjoin(prompt, " >");
+	return (prompt);
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -20,7 +128,7 @@ int	main(int ac, char **av, char **env)
 		return (0);
 	while (1)
 	{
-		line = readline(PROMPT);
+		line = readline(ft_get_prompt(env));
 		if (!line)
 			break;
 		add_history(line);
