88e1e37ce2911e342ed206d83ba6a819e1e9a941
Sku <alexandre.hamad@hotmail.fr>
2026-02-18 14:49:57 +0100

    add comment in built-in

--- src/cmd/env.c
+++ src/cmd/env.c
@@ -6,13 +6,15 @@
 /*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 10:58:03 by alehamad          #+#    #+#             */
-/*   Updated: 2026/02/18 02:48:09 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/02/18 14:47:20 by alehamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
 // env with no options or arguments
+// diff avec export | env affiche les var avec une valeur
+// export affiche toute les var meme sans valeur en ordre ASCII
 
 void	ft_env(t_data *data)
 {
--- src/cmd/export.c
+++ src/cmd/export.c
@@ -6,8 +6,11 @@
 /*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 10:59:58 by alehamad          #+#    #+#             */
-/*   Updated: 2026/02/18 02:41:30 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/02/18 14:48:24 by alehamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+// export affiche toute les var meme sans valeur en ordre ASCII
+// recrer un nouveau tableau de var et les trier print ce tableau sans toucher a env
