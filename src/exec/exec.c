e7ebef97dc4b7d814fd530c6450b49c00083bb71
Tarek KHIDER <tarek@khider.fr>
2026-02-18 23:01:18 +0100

    added path resolution

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tkhider <tkhider@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 14:47:02 by tkhider           #+#    #+#             */
-/*   Updated: 2026/02/18 02:10:18 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/02/18 22:56:23 by tkhider          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,27 @@ int	exec_cmd(t_cmd *command, t_data *data)
 {
 	pid_t	pid;
 	int		status;
+	char	*valid_path;
 
+	if (!command || !command->arg_cmd || !command->arg_cmd[0])
+		return (0);
+	valid_path = findvalidpath(data->my_env, command->arg_cmd[0]);
 	pid = fork();
-	if(pid == 0)
+	if (pid == 0)
 	{
-		execve(command->arg_cmd[0], command->arg_cmd, data->my_env);
-		perror("exeve :");
-		exit(EXIT_FAILURE);
+		if (!valid_path)
+		{
+			ft_putstr_fd("Command not found \n", 2);
+			exit(127);
+		}
+		execve(valid_path, command->arg_cmd, data->my_env);
+		perror("exeve");
+		exit(126);
 	}
-	else if(pid > 0)
+	else if (pid > 0)
 		waitpid(pid, &status, 0);
 	else
-		perror("fork :");
-	return 0;
+		perror("fork");
+	free(valid_path);
+	return (0);
 }
---
+++ src/exec/path_resolution.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path_resolution.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkhider <tkhider@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 22:34:25 by tkhider           #+#    #+#             */
+/*   Updated: 2026/02/18 23:00:41 by tkhider          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+static int	isabsolute(char *s)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == '/')
+			return (1);
+		s++;
+	}
+	return (0);
+}
+char	*getpath(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			return (env[i] + 5);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	*testpath(char **paths, char *binary)
+{
+	int		i;
+	char	*part;
+	char	*full;
+
+	i = 0;
+	while (paths[i])
+	{
+		part = ft_strjoin(paths[i], "/");
+		full = ft_strjoin(part, binary);
+		free(part);
+		if (access(full, F_OK | X_OK) == 0)
+			return (full);
+		free(full);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*findvalidpath(char **env, char *binary)
+{
+	char	*path;
+	char	**paths;
+
+	if (!binary || *binary == '\0')
+		return (NULL);
+	if (isabsolute(binary))
+	{
+		if (access(binary, F_OK | X_OK) == 0)
+			return (ft_strdup(binary));
+		return (NULL);
+	}
+	path = getpath(env);
+	if (!path)
+		return (NULL);
+	paths = ft_split(path, ':');
+	path = testpath(paths, binary);
+	free_tab(paths);
+	return (path);
+}
