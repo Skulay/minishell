4829384ff920aa05b2dc0cb513b255a900bab8ec
Tarek KHIDER <tarek@khider.fr>
2026-02-20 02:15:38 +0100

    splitting redirection_manager into sub-functions

--- src/redirections/redir.c
+++ src/redirections/redir.c
@@ -6,34 +6,44 @@
 /*   By: tkhider <tkhider@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 22:52:33 by tkhider           #+#    #+#             */
-/*   Updated: 2026/02/19 23:22:52 by tkhider          ###   ########.fr       */
+/*   Updated: 2026/02/20 02:14:11 by tkhider          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	redirection_manager(t_redir *redir)
+static int	ft_append(t_redir *redir)
 {
 	int	fd;
 
+	fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd < 0)
+		return (perror(redir->file), 1);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	ft_redir_out(t_redir *redir)
+{
+	int	fd;
+
+	fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd < 0)
+		return (perror(redir->file), 1);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+int	redirection_manager(t_redir *redir)
+{
 	while (redir)
 	{
 		if (redir->type == REDIR_OUT)
-		{
-			fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd < 0)
-				return (perror(redir->file), 1);
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			ft_redir_out(redir);
 		else if (redir->type == APPEND)
-		{
-			fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (fd < 0)
-				return (perror(redir->file), 1);
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			ft_append(redir);
 		redir = redir->next;
 	}
 	return (0);
