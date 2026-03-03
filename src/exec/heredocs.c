0c4a9736ee4cdb922a2c1ce466af1e7a87005518
Sku <alexandre.hamad@hotmail.fr>
2026-03-03 17:44:52 +0100

    ok

--- src/exec/heredocs.c
+++ src/exec/heredocs.c
@@ -6,7 +6,7 @@
 /*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 03:53:22 by tkhider           #+#    #+#             */
-/*   Updated: 2026/03/03 17:40:46 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/03/03 17:44:31 by alehamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,46 @@ char	*ft_heredoc_expand(char *line)
 
 // cherche la prochaine value vis a vis de la nouvelle string si il y'en a une
 
-//        TEST
-
+//logique pour expand 1 par 1
 // $USER test $USER test $USER
 // sku test $USER test $USER
 // sku test sku test $USER
 // sku test sku test sku
 
+//        TEST
+
+// input > test$USERufhrehu
+// out   > test
+
 //        TEST2
 
-// test$USERufhrehu
-// test
+// input > test $USERufhrehu
+// out   > test
 
 //        TEST3
 
-// test $USERufhrehu
-// test
+// input > test $USER ufhrehu
+// out   > test sku ufhrehu
 
 //        TEST4
 
-// test $USER ufhrehu
-// test sku ufhrehu
+// input > test $USER$USER
+// out   > test skusku
 
 //        TEST5
 
-// test $USER$USER
-// test skusku
+// input > test $USER$PWDEW
+// out   > test sku
 
 //        TEST6
 
-// test $USER$PWDEW
-// test sku
+// input > test $USER$PWDEW salut
+// out   > test sku salut
 
 //        TEST7
 
-// test $USER$PWDEW salut
-// test sku salut
-
-// test $USERFEWFWE$PWD salut
-// test /home/sku/projet/minishell salut
+// input > test $USERFEWFWE$PWD salut
+// out   > test /home/sku/projet/minishell salut
 
 // donc faire un tableaux split sur espace
 // a chaque tab[i] si c'est une var l'expand et si on trouve pas return le debut du tableaux
